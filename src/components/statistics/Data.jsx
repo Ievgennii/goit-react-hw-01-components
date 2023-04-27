ebb26e0cd154d24bb3b86c72a687889a57f49be2
Ievgennii <simonovievgenii@gmail.com>
2023-04-27 23:40:39 +0300

    add styles statistics

--- src/components/statistics/Data.jsx
+++ src/components/statistics/Data.jsx
@@ -1,17 +1,24 @@
 import PropTypes from 'prop-types';
+import css from './statistics.module.css';
+import getRandomHexColor from './utils/getRandomHexColor';
 
 function Data({ label, percentage }) {
+  const itemStyles = {
+    display: 'grid',
+
+    backgroundColor: getRandomHexColor(),
+  };
   return (
-    <>
-      <span class="label">{label}</span>
-      <span class="percentage">{percentage} %</span>
-    </>
+    <div style={itemStyles}>
+      <span className={css.label}>{label}</span>
+      <span className={css.percentage}>{percentage}%</span>
+    </div>
   );
 }
 
 Data.propTypes = {
-  label:PropTypes.string,
-  percentage:PropTypes.number,
-}
+  label: PropTypes.string,
+  percentage: PropTypes.number,
+};
 
 export default Data;
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import Data from './Data';
+import css from './statistics.module.css';
 
 function Statistics({ data = [] }) {
   return (
-    <ul class="stat-list">
+    <ul className={css.statList}>
       {data.map(dataItem => (
-        <li class="item" key={dataItem.id}>
-          <Data label={dataItem.label} percentage={dataItem.percentage}/>
+        <li className={css.item} key={dataItem.id}>
+          <Data label={dataItem.label} percentage={dataItem.percentage} />
         </li>
       ))}
     </ul>
